417e2c5de1130058e74ef90b4f8084596b31bbdc
Dmitry <75675320+Skym4r@users.noreply.github.com>
2023-07-10 17:05:02 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="", db="bots", charset='utf8')
+db = MySQLdb.connect(host="хост", user="имя_позьзователя", passwd="пароль", db="название_бд", charset='utf8')
 
 cursor = db.cursor()
 print(cursor)
@@ -9,11 +9,11 @@ class DB:
     
     @staticmethod
     def get_by_id(user_id,phone,curier):
-        sql =cursor.execute("""SELECT * FROM danii WHERE phone = ('%(phone)s')"""%{"phone":phone})
+        sql =cursor.execute("""SELECT * FROM название_таблицы WHERE phone = ('%(phone)s')"""%{"phone":phone})
         result=cursor.fetchall()
         print(result)
         if len(result) == 0: 
-            sql = """INSERT INTO danii (id_chat, phone, curier)  VALUES  ('%(id_chat)s', '%(phone)s', '%(curier)s')"""%{"id_chat":user_id,"phone":phone, "curier":curier}
+            sql = """INSERT INTO название_таблицы (id_chat, phone, curier)  VALUES  ('%(id_chat)s', '%(phone)s', '%(curier)s')"""%{"id_chat":user_id,"phone":phone, "curier":curier}
             cursor.execute(sql)
             print(cursor.execute(sql))
             db.commit()
@@ -21,7 +21,7 @@ class DB:
              
     @staticmethod
     def proverk_id(chat):
-        sql =cursor.execute("""SELECT * FROM danii WHERE id_chat = ('%(id_chat)s')"""%{"id_chat":chat})
+        sql =cursor.execute("""SELECT * FROM название_таблицы WHERE id_chat = ('%(id_chat)s')"""%{"id_chat":chat})
         #cursor.execute(sql)
         
         result=cursor.fetchall()        
